fedac44befb06b3f3ae1a145a417d27418fbfaf5
Zhenyu Zhu <zhuzhenyu1994@hotmail.com>
2018-12-03 22:15:00 -0500

    calculate fit

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Triangle extends Polygon {
     double maxMutateRate;
@@ -26,7 +27,7 @@ public class Triangle extends Polygon {
 
         getColorFromGene(gene);
         this.genotype = gene;
-        
+
     }
 
     private void getColorFromGene(Genotype gene) {
@@ -36,6 +37,33 @@ public class Triangle extends Polygon {
         float a = gene.getColorChromo().getAlpha();
 
         this.color = new Color(r, g, b, a);
+    }
+
+    private double calFitness(BufferedImage target) {
+        double res = 0;
+        for (int x = 0; i < 256; i++) {
+            for (int y = 0; y < 256; y++) {
+                if (this.contains(x, y)) {
+                    res += calFitHelper(target, x, y);
+                }
+            }
+        }
+        return res;
+    }
+
+    private double calFitHelper(BufferedImage target, int x, int y) {
+        int tarColor = target.getRGB(x, y);
+        int tarA = (tarColor >> 24) & 0xFF;
+        int tarR = (tarColor >> 16) & 0xFF;
+        int tarG = (tarColor >> 8) & 0xFF;
+        int tarB = tarColor & 0xFF;
+
+        int a = genotype.getColorChromo().getAlpha();
+        int r = genotype.getColorChromo().getRed();
+        int g = genotype.getColorChromo().getGreen();
+        int b = genotype.getColorChromo().getBlue();
+
+        return Math.pow((a - tarA), 2) + Math.pow((r - tarR), 2) + Math.pow((g - tarG), 2) + Math.pow((b - tarB), 2);
     }
 
     private void mutate(Triangle parent) {
